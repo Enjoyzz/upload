5948dbe068946b7abc5b55701b1a72ca2c309629
enjoys <deadly.info@gmail.com>
2022-07-24 14:32:07 +0300

    added comment to RuleInterface.php

--- src/RuleInterface.php
+++ src/RuleInterface.php
@@ -6,9 +6,18 @@ declare(strict_types=1);
 namespace Enjoys\Upload;
 
 
+use Enjoys\Upload\Exception\RuleException;
 use Psr\Http\Message\UploadedFileInterface;
 
 interface RuleInterface
 {
+    /**
+     * If the check failed, you need to throw an exception \Upload\Exception\Rule,
+     * if successful, return nothing (void)
+     *
+     * @param UploadedFileInterface $file
+     * @return void
+     * @throws RuleException
+     */
     public function check(UploadedFileInterface $file): void;
 }
